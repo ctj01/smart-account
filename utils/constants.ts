bebaa9d8c4fd2d576c56ca1a03086e814aa6ad7d
CristianMendoza <cristian.mendoza@prestti.com>
2024-07-20 18:17:57 -0500

    create account for owners

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat"
-
+const { Wallet } = ethers;
 export const parseEther = ethers.parseEther;
 
 export const AddressZero = "0x0000000000000000000000000000000000000000";
@@ -7,3 +7,4 @@ export const HashZero = "0x00000000000000000000000000000000000000000000000000000
 export const ONE_ETH = parseEther('1')
 export const TWO_ETH = parseEther('2')
 export const FIVE_ETH = parseEther('5')
+export const createAccountOwner = () => Wallet.createRandom().connect(ethers.provider);
